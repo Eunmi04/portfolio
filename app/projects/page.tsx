1c8908a90f17543914637ef8077825137917f92a
Eunmi <eunmi041119@gmail.com>
2024-10-20 00:37:24 +0900

    first commit

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,12 +5,12 @@ export default function Projects() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8">
       <h1 className="text-3xl font-bold text-center mb-8">My Projects</h1>
-      
+
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl w-full">
         <div className="bg-white p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <div className="flex items-center">
             <Image
-              src="/project1.jpg"
+              src="/[[...image]]/project1.jpg"
               alt="Project1 Picture"
               className="w-32 h-32 mr-4 rounded-lg shadow-md"
               width={200}
@@ -28,7 +28,7 @@ export default function Projects() {
         <div className="bg-white p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <div className="flex items-center">
             <Image
-              src="/2048.jpg"
+              src="/[[...image]]/2048.jpg"
               alt="Project2 Picture"
               className="w-32 h-32 mr-4 rounded-lg shadow-md"
               width={200}
@@ -37,7 +37,8 @@ export default function Projects() {
             <div>
               <h2 className="text-2xl font-semibold">2048</h2>
               <p className="text-lg text-gray-600">
-                같은 숫자의 타일을 결합하며 2048 타일에 도달하는 것이 목표인 게임
+                같은 숫자의 타일을 결합하며 2048 타일에 도달하는 것이 목표인
+                게임
               </p>
             </div>
           </div>
@@ -46,7 +47,7 @@ export default function Projects() {
         <div className="bg-white p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <div className="flex items-center">
             <Image
-              src="/dream.png"
+              src="/[[...image]]/dream.png"
               alt="Project3 Picture"
               className="w-32 h-32 mr-4 rounded-lg shadow-md"
               width={200}
